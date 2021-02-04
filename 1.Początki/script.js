54b93cf2c5df83e8b78d4a2184a417d324399326
Mrozior121 <50276787+Mrozior121@users.noreply.github.com>
2021-02-04 19:45:43 +0100

    Update script.js
    Done

---
+++ 1.Początki/script.js
@@ -0,0 +1,12 @@
+//zad1
+console.log("Michał");
+
+//zad2
+console.log(12+32);
+console.log(52/16); // zmiennoprzecinkowa
+console.log(5*2);
+
+//zad3
+console.log("Hello World");
+//console.log(Hello World); // bład, nieznana zmienna
+// console.log("Hello World); //bład, niezakończony string
