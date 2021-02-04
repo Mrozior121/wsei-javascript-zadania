f427f05fd840807e0af0c0b5a50739e06e44268d
Mrozior121 <50276787+Mrozior121@users.noreply.github.com>
2021-02-04 19:51:21 +0100

    Done

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,24 @@
+// Zad 1:
+
+let number = 2;
+let string = "dwa";
+let stringNumConcat = number + string;
+let logicValue = 5 < 1;
+let special = null;
+
+console.log(number, string, stringNumConcat, logicValue, special);
+
+// Zad 2:
+
+let num0 = 16;
+let num1 = 4;
+let sum = 0;
+sum = num0 + num1;
+console.log(sum);
+
+// Zad 3:
+
+let unknown;
+console.log();
+
+// Zwracana jest wartość undefined, ponieważ zmienna nie ma przypisanej wartości
